65b81ba55a622c930f52b3ad31583d2844a29ec0
johnharan <johnharan@users.noreply.github.com>
2015-09-18 22:04:42 +0100

    Update TestClass.java

--- GitTestProject/src/TestClass.java
+++ GitTestProject/src/TestClass.java
@@ -3,6 +3,7 @@
 public class TestClass {
 	
 	/*
+	22:04
 	exercise from codingbat.com
 	Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, 
 	or return 0 if neither is in that range.
